fdf2e8195f2bd34a28f7838da982a47f94e079e6
Sorcha Abel <sorcha.abel@cultureamp.com>
2022-09-17 14:12:01 +1000

    first commit

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Types
+  class AuthorType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String
+    field :last_name, String
+    field :dob, Integer
+    field :is_alive, Boolean
+  end
+end
